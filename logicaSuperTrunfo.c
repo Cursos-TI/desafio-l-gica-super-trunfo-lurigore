57db460e25b4a125e59ce6641a6f365cdc9d5e4a
lurigore <153800413+lurigore@users.noreply.github.com>
2025-07-25 10:32:12 +0000

    Todas alterções até agora

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 char nomeEstado1[50];
 char codigo1[4];
 unsigned long int populacao1;
